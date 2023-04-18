c7346057a692168d064d100deaaf0a314ce0e4b0
efth2007 <efth2007@hotmail.com>
2023-04-18 13:58:30 +0200

    Change host 2

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
     user:'postgres',
-    host: '0.0.0.0',
+    host: '127.0.0.1',
     database: 'contacts',
     password: '1077892',
     port: 5432,
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const contactRoutes = require('./src/contact/routes');
 
 const app = express();
-const port = 4000;
+const port = 3000;
 
 app.use(express.json());
 
