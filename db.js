55dc6c47e2895584292c661d33a37ad22ab86177
efth2007 <efth2007@hotmail.com>
2023-04-20 16:04:03 +0200

    db configuration

--- db.js
+++ db.js
@@ -1,11 +1,22 @@
 const Pool = require('pg').Pool;
+require('dotenv').config()
 
 const pool = new Pool({
-    user:'postgres',
-    host: '127.0.0.1',
-    database: 'contacts',
-    password: '1077892',
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.HOST,
+    database: process.env.DB,
+    password: process.env.PW,
+    port: process.env.PORT,
+    ssl: true,
 });
 
 module.exports = pool;
+
+
+// const pool = new Pool({
+//     user: 'postgres',
+//     host: process.env.HOST,
+//     database: process.env.DB,
+//     password: process.env.PW,
+//     port: process.env.PORT,
+// });
--- server.js
+++ server.js
@@ -3,14 +3,15 @@ const contactRoutes = require('./src/contact/routes');
 require("dotenv").config();
 
 const app = express();
-const port = 3000;
+const port = 8000;
 
 app.use(express.json());
 
 app.get("/", (req, res)=>{
-    res.send(`Yoo ${process.env.MESSAGE}`)
+    res.send(`Yoo ${process.env.MESSAGE} !!!!!`)
+   // res.send(`second line yaall`)
 })
 
 app.use('/api/v1/contacts', contactRoutes )
 
-app.listen(port, () => console.log("LISTENING....d."))
+app.listen(port, () => console.log(`LISTENING ON.....${port}`))
