641186900e0de172f9c77b2f51904c365a258183
efth2007 <efth2007@hotmail.com>
2023-04-21 15:47:11 +0200

    SSL setting

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ const pool = new Pool({
     database: process.env.DB,
     password: process.env.PW,
     port: process.env.PORT,
-    ssl: true,
+    ssl: {rejectUnauthorized: false},
 });
 
 module.exports = pool;
